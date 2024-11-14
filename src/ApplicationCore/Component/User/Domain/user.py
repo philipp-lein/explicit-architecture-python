e9bd2b7add290c883e65d522eae026c3ffb5605f
Philipp Lein <Philipp.Lein@neocx.de>
2024-11-14 13:38:57 +0100

    Remove not needed function in Model

--- src/ApplicationCore/Component/User/Domain/user.py
+++ src/ApplicationCore/Component/User/Domain/user.py
@@ -2,10 +2,5 @@ class User(object):
     def __init__(self, username: str):
         self.username = username
 
-    @staticmethod
-    def create_user(username: str) -> "User":
-        user = User(username)
-        return user
-
     def to_dict(self) -> dict:
         return {"username": self.username}
